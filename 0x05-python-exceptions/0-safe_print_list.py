98bd860e002d2ae3ce5347d6720c76e63171e152
Ntsika2 <ntsikacaesar@gmail.com>
2024-01-24 01:01:48 -0800

    Removed space

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
+def safe_print_list(my_list=[], x = 0):
     count = 0
 
     try:
         for i in range(x):
-            print(my_list[i], end=' ')
+            print(my_list[i], end="")
             count += 1
     except IndexError:
         pass
     finally:
-        print()  # Print a new line after all elements are printed
+        print("")  # Print a new line after all elements are printed
         return count
