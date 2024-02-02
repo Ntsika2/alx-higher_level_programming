f3afe423c1783920501b072047802bffbd948527
= <=>
2024-02-01 21:47:42 -0800

    0. Integers addition

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+def add_integer(a, b=98):
+    # Check if a is an integer or float
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer or float")
+
+    # Check if b is an integer or float
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer or float")
+
+    # Cast a and b to integers if they are floats
+    a = int(a)
+    b = int(b)
+
+    # Return the addition of a and b
+    return a + b
